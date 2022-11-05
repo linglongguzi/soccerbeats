9242bb47ff82e91bc39ed9d41ff8928611b04ea0
jessthez <jess.ziyue.zhang@gmail.com>
2022-11-05 14:37:03 -0700

    item title update

--- nftstore/src/components/ItemCard.jsx
+++ nftstore/src/components/ItemCard.jsx
@@ -4,9 +4,9 @@ import clothImg from '../assets/model4.png';
 const ItemCard = ({img})=>{
     return(
         <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-4 m-2">
-            <div class="text-pink">#231</div>
+            <div class="text-grey-200 text-center mb-8 bg-slate-300 rounded-full">#231</div>
             <img src={img} alt="jersey" class="bg-transparent"></img>
-            <div>lv2</div>
+            <div class="my-4 text-center">lv2</div>
             <PriceCheckoutButton></PriceCheckoutButton>
         </div>
     );
@@ -14,9 +14,9 @@ const ItemCard = ({img})=>{
 
 const PriceCheckoutButton = () => {
     return(
-        <div class="container bg-blue-200 rounded-full py-1">
-            <span class="inline-block px-4"> 0.01ETH </span>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Buy</button>
+        <div class="container bg-blue-200 rounded-full py-1 items-stretch">
+            <span class="inline-block px-4"> 0.01 ETH </span>
+            <button class="bg-blue-500 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded-full">Buy</button>
         </div>
     );
 }
