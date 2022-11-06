0764ff1b0e33497e21fcd1f2a8ddc5efa41ece7e
Sigrid-spec <117507241+Sigrid-spec@users.noreply.github.com>
2022-11-06 14:19:58 +0800

    update my asset

--- nftstore/src/modules/myassets/index.js
+++ nftstore/src/modules/myassets/index.js
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, {useState , useEffect, Suspense} from 'react';
 import clothImg from '../../assets/model4.png';
 import girlImg from '../../assets/model3.png';
 import AssetCard from '../../components/AssetCard';
-import { GetAddress } from "../../components/Web3";
 
 import './index.css';
 
-class MyAssets extends React.Component {
-  renderWalletNotConnected = () => (
+function MyAssets () {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    }
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const renderWalletNotConnected = () => (
     <div>Connect wallet to see your assets</div>
   );
 
-  renderWalletConnected = ()=> (
+  const renderWalletConnected = ()=> (
       <div className="container py-5">
           <div class="grid grid-cols-4 gap-6 bg-white">
             <AssetCard img={girlImg} isAvatar={true}></AssetCard>
             <AssetCard img={clothImg}></AssetCard>
           </div>
       </div>
-  );
+  )
 
-  render(){
-    return(
-      <div>
-        { GetAddress === "" ? (this.renderWalletNotConnected()) : (this.renderWalletConnected())}
-      </div>
-    )}
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  return(
+    <Suspense>
+      { currentAccount === "" ? (renderWalletNotConnected()) : (renderWalletConnected())}
+    </Suspense>
+  )
 }
 export default MyAssets;
