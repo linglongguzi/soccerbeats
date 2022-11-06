77bafdf0d03bf51bf9c6aabff9ed030710e283d0
linglong <linglong@blockus.company>
2022-11-06 07:25:06 -0800

    add more accessories

--- nftstore/src/modules/accessories/index.js
+++ nftstore/src/modules/accessories/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ItemCard from '../../components/ItemCard';
 import './index.css';
 import clothImg from '../../assets/model4.png';
+import hatImg from '../../assets/hat.jpeg';
+import sneakerImg from '../../assets/sneaker.png';
 
 class Accessories extends React.Component {
   render() {
@@ -9,9 +11,13 @@ class Accessories extends React.Component {
       <div className="container py-5">
           <div class="grid grid-cols-4 gap-6">
             <ItemCard img={clothImg}></ItemCard>
+            <ItemCard img={sneakerImg}></ItemCard>
+            <ItemCard img={hatImg}></ItemCard>
             <ItemCard img={clothImg}></ItemCard>
+            <ItemCard img={sneakerImg}></ItemCard>
+            <ItemCard img={hatImg}></ItemCard>
             <ItemCard img={clothImg}></ItemCard>
-            <ItemCard img={clothImg}></ItemCard>
+            <ItemCard img={sneakerImg}></ItemCard>
           </div>
       </div>
     )
